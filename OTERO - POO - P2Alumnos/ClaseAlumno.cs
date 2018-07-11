25c171bb04774bd7449b31900e7a4300cba5ae13
Zim92 <sebastian.otero@live.com.ar>
2018-07-11 13:34:36 -0300

    first commit

---
+++ OTERO - POO - P2Alumnos/ClaseAlumno.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OTERO___POO___P2Alumnos
+{
+    class ClaseAlumno
+    {
+        List<ClaseMateria> listaMaterias;
+        private int legajo;
+        private string nombre;
+        private string apellido;
+        
+        #region "Constructores"
+        public ClaseAlumno()
+        {
+            listaMaterias = new List<ClaseMateria>();
+        }
+        public ClaseAlumno(int pLegajo, string pNombre, string pApellido)
+        {
+            Legajo = pLegajo;
+            Nombre = pNombre;
+            Apellido = pApellido;
+        }
+        #endregion
+
+        #region "Propiedades"
+        public int Legajo
+        {
+            get { return legajo; }
+            set { legajo = value; }
+        }
+        public string Nombre
+        {
+            get { return nombre; }
+            set { nombre = value; }
+        }
+        public string Apellido
+        {
+            get { return apellido; }
+            set { apellido = value; }
+        }
+        #endregion
+    }
+}
---
+++ OTERO - POO - P2Alumnos/ClaseMateria.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OTERO___POO___P2Alumnos
+{
+    class ClaseMateria
+    {
+        public ClaseMateria()
+        {
+
+        }
+
+        private int codigo;
+        private string denominacion;
+        private decimal nota;
+
+        public int Codigo
+        {
+            get { return codigo; }
+            set { codigo = value; }
+        }
+
+        public string Denominacion
+        {
+            get { return denominacion; }
+            set { denominacion = value; }
+        }
+
+        public decimal Nota
+        {
+            get { return nota; }
+            set { nota = value; }
+        }
+
+    }
+}
